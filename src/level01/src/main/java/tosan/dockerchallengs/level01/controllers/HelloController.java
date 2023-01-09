19801c61a0b7bab83958de4bb650d124b951a3ab
S.Salimzadeh <s.salimzadeh@tosan.com>
2023-01-09 15:28:08 +0330

    create HelloController and test to work

---
+++ src/level01/src/main/java/tosan/dockerchallengs/level01/controllers/HelloController.java
@@ -0,0 +1,19 @@
+package tosan.dockerchallengs.level01.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Sajad Salimzadeh
+ * @since 1/9/2023
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello";
+    }
+}
