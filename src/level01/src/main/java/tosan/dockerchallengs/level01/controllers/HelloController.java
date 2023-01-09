9f86f0ac5f53a5da3530eed4646eca5589f5418d
S.Salimzadeh <s.salimzadeh@tosan.com>
2023-01-09 15:29:35 +0330

    add hello to user with name or stranger implementation

--- src/level01/src/main/java/tosan/dockerchallengs/level01/controllers/HelloController.java
+++ src/level01/src/main/java/tosan/dockerchallengs/level01/controllers/HelloController.java
@@ -2,6 +2,7 @@ package tosan.dockerchallengs.level01.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,7 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping
-    public String sayHello() {
-        return "Hello";
+    public String sayHello(@RequestParam(required = false) String name) {
+        if(name == null || name.isEmpty()) {
+            return "Hello Stranger";
+        }
+
+        return "Hello " + name;
     }
 }
