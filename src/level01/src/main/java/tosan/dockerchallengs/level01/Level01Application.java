d0842df64e407075dfc8b9ddbc228b764c6e067b
S.Salimzadeh <s.salimzadeh@tosan.com>
2023-01-09 15:26:22 +0330

    create project level01 with springboot initializer

---
+++ src/level01/src/main/java/tosan/dockerchallengs/level01/Level01Application.java
@@ -0,0 +1,13 @@
+package tosan.dockerchallengs.level01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Level01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Level01Application.class, args);
+	}
+
+}
